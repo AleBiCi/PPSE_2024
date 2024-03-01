7e912ea2c88be6da1686488873bb20f770e72d5e
Vise22 <135508170+Vise22@users.noreply.github.com>
2024-03-01 13:01:10 +0100

    Update servo_control.cpp

--- SunScanner/src/servoLib/servo_control.cpp
+++ SunScanner/src/servoLib/servo_control.cpp
@@ -1,5 +1,6 @@
 #include "servoLib/servo_control.h"
 
+/* Costruttore */
 ServoController::ServoController(int pin_az, int pin_el, int current_p, int voltage_p):
 	servoAz(360,pin_az),
 	servoEl(180,pin_el){
@@ -9,30 +10,33 @@ ServoController::ServoController(int pin_az, int pin_el, int current_p, int volt
 	pinMode(voltage_pin,INPUT);
 }
 
+/* point to sun automatically */
 void ServoController::auto_positioning(tm* time_info_ptr, double Lat, double Lon, double Alt, double compass_val, double* Az, double* El){
 	int azpos;
 	SolarAzEl(time_info_ptr, Lat, Lon, Alt, Az, El);
-	azpos=*(Az); //  - (int)(compass + 360) % 360;
+	azpos=*(Az);
+
+	/* compass adjustments */
 	if(compass_val>180){
 		azpos+=(360-compass_val);
 	}else{
 		azpos-=compass_val;
 	}
-  /* these adjustments work if the 0 angle of the servoAz face same direction of compass */
+	
+  	/* these adjustments work if the 0 angle of the servoAz face same direction of compass */
 	servoAz.set_servo(azpos);
 	servoEl.set_servo(*(El));
 }
 
+/* ADC read of voltage and current from solar panel */
 double ServoController::get_voltage(){
 	int voltage = analogRead(voltage_pin);
 	return(voltage);
 }
 
-double ServoController::get_current(){
-	int current = analogRead(current_pin);
-	return(current);
-}
-
+/* Function that point at the most power efficient point  */
+/* step => how many degrees each servo move per iteration */
+/* n_iter => how many step are taken in each direction */
 void ServoController::max_power_pos(double step, int n_iter){
 	int pow, max_pow_az, max_pow_el;
 	int i;
