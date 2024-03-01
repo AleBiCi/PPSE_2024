e3ef2158e9cad3040fbfe294f80e2ea554eaf0ee
Vise22 <135508170+Vise22@users.noreply.github.com>
2024-03-01 12:56:20 +0100

    Update servo_control.h

--- SunScanner/include/servoLib/servo_control.h
+++ SunScanner/include/servoLib/servo_control.h
@@ -16,12 +16,10 @@ public:
 	/* Function that point at the most power efficient point  */
 	/* step => how many degrees each servo move per iteration */
 	/* n_iter => how many step are taken in each direction */
-  /* step_delay_ms => delay betwen */
 	void max_power_pos(double step, int n_iter);
 
 	/* ADC read of voltage and current from solar panel */
 	double get_voltage();
-	double get_current();
 	
 	/* point to sun automatically */
 	void auto_positioning(tm* time_info_ptr, double Lat, double Lon, double Alt, double compass_val, double* Az, double* El);
