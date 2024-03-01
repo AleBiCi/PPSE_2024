0d54dc057f76e8ac47a970ff9ba5422a3b426584
Vise22 <135508170+Vise22@users.noreply.github.com>
2024-03-01 10:52:11 +0100

    Update menu.h

--- SunScanner/include/oled/menu.h
+++ SunScanner/include/oled/menu.h
@@ -8,22 +8,28 @@
 
 //float voltage_min = 100,voltage_max=0;
 
+/* Lettura tensione */
 float Tensione(int pin_tensione);
 
+/* Setup del display */
 void setup_disp();
 
 void clear_disp();
 
+/* Funzione per disegnare una singola opzione del menu */
 void drawMenuElement(int x, int y, const unsigned char *bitmap, char* name);
 
+/* menu iniziale a profondità 1 */
 void drawMenu1(int i);
 
+/*Menu di secondo livello: dopo aver premuto il pulsante dx*/
 void drawMenu2(int i, int sel, int pin_v, float phi, float teta);
 
+/* Informazioni a schermo dopo aver selezionato una delle tre move */
 void drawMenu3(int i);
 
+/* Funzione che seleziona il menu da printare */
 void SelezioneMenu(int i, int j, int sel, int pin_v, float phi, float teta);
 
-int menu_keep_range_1_3(int val);
 
 #endif
