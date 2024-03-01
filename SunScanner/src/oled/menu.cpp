550f166afdc7083b0bf31d165786e447fb9aceef
Vise22 <135508170+Vise22@users.noreply.github.com>
2024-03-01 10:48:25 +0100

    Update menu.cpp

--- SunScanner/src/oled/menu.cpp
+++ SunScanner/src/oled/menu.cpp
@@ -1,5 +1,6 @@
 #include "oled/menu.h"
 
+/* dichiarazione delle bitmap del menu */
 extern const unsigned char bitmap_specs [] PROGMEM;
 extern const unsigned char bitmap_GPS [] PROGMEM;
 extern const unsigned char bitmap_move [] PROGMEM;
@@ -23,20 +24,16 @@ const unsigned char* home_menu[4] = { bitmap_specs, bitmap_GPS, bitmap_move, bit
 char *home_menu_names[4] = { (char*)"Specs", (char*)"GPS", (char*)"Move", (char*)"History"};
 char *home_menu_move[4] = { (char*)"Automatic", (char*)"Zenit", (char*)"Manual"};
 
+/* Lettura tensione */
 float Tensione(int pin_tensione) {
   int valore_letto = analogRead(pin_tensione);
-  float v_out = valore_letto * (5.0 / 1023.0);
-  //if(v_out>voltage_max){
-  //  voltage_max=v_out;
-  //}
-  //if(v_out<voltage_min){
-  //  voltage_min=v_out;
-  //}
+  float v_out = valore_letto * (5.0 / 1023.0);  //conversione dal livello di analogRead a tensione effettiva
   return v_out;
 }
 
-Adafruit_SSD1306 disp(128, 64, &Wire);
+Adafruit_SSD1306 disp(128, 64, &Wire);  //Dichiarazione display
 
+/* Setup del display */
 void setup_disp(){
   if(!disp.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     // Serial.println(F("SSD1306 allocation failed"));
@@ -50,16 +47,17 @@ void clear_disp(){
   disp.clearDisplay();
 }
 
-
+/* Funzione per disegnare una singola opzione del menu */
 void drawMenuElement(int x, int y, const unsigned char *bitmap, char* name) {
-  disp.drawBitmap(x, y, bitmap, 16, 16, WHITE);
+  disp.drawBitmap(x, y, bitmap, 16, 16, WHITE);  //icona da printare
   disp.setTextSize(1);
   disp.setTextColor(WHITE);
   disp.setCursor(x + 28, y + 3);
-  disp.print(name);
+  disp.print(name);  //testo printare
   disp.display();
 }
 
+/* Schermata di caricamento: 3 quadratini che appaiono in sequenza */
 void WaitMove() {
   disp.clearDisplay();
 
@@ -78,57 +76,57 @@ void WaitMove() {
 
  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+/* menu iniziale a profondità 1 */
 void drawMenu1(int i) {
   //display.clearDisplay();
   switch (i) {
-    /*case 1:
+    case 1 /*SPECS*/:
       drawMenuElement(4, 2, bitmap_history, home_menu_names[3]);
       disp.drawRect(1, 22, 126, 20, WHITE);
       drawMenuElement(4, 24, bitmap_specs, home_menu_names[0]);
       drawMenuElement(4, 48, bitmap_GPS, home_menu_names[1]);
-      break;*/
+      break;
 
-    case 2:
-      //drawMenuElement(4, 2, bitmap_specs, home_menu_names[0]);
+    case 2 /*GPS*/:
+      drawMenuElement(4, 2, bitmap_specs, home_menu_names[0]);
       disp.drawRect(1, 22, 126, 20, WHITE);
       drawMenuElement(4, 24, bitmap_GPS, home_menu_names[1]);
       drawMenuElement(4, 48, bitmap_move, home_menu_names[2]);
       break;
 
-    case 3:
+    case 3 /*move*/:
       drawMenuElement(4, 2, bitmap_GPS, home_menu_names[1]);
       disp.drawRect(1, 22, 126, 20, WHITE);
       drawMenuElement(4, 24, bitmap_move, home_menu_names[2]);
-      //drawMenuElement(4, 48, bitmap_history, home_menu_names[3]);
+      drawMenuElement(4, 48, bitmap_history, home_menu_names[3]);
       break;
 
-    /*case 4:
+    case 4 /*history*/:
       drawMenuElement(4, 2, bitmap_move, home_menu_names[2]);
       disp.drawRect(1, 22, 126, 20, WHITE);
       drawMenuElement(4, 24, bitmap_history, home_menu_names[3]);
       drawMenuElement(4, 48, bitmap_specs, home_menu_names[0]);
-      break;*/
+      break;
   }
 }
 
+/*Menu di secondo livello: dopo aver premuto il pulsante dx*/
 void drawMenu2(int i, int sel, int pin_v, float phi, float teta) {
+  //Dichiarazione di variabili che vengono poi printate a schermo
   char tensione_string[5];
   char phi_string[5];
   char theta_string[5];
   char max_tension_string[5];
   char min_tension_string[5];
-  //display.clearDisplay();
 
   switch (i) {
-    //specs
-   /* case 1:
+    case 1 /*specs*/:
       
       dtostrf(Tensione(pin_v), 5, 2, tensione_string);
       drawMenuElement(4, 24, bitmap_icon_v, tensione_string);
-      break;*/
+      break;
 
-    //GPS
-    case 2:
+    case 2/*GPS*/:
       dtostrf(phi, 5, 2, phi_string);
       drawMenuElement(4, 2, bitmap_icon_phi, phi_string);
 
@@ -136,11 +134,10 @@ void drawMenu2(int i, int sel, int pin_v, float phi, float teta) {
       drawMenuElement(4, 24, bitmap_icon_theta, theta_string);
       break;
 
-    //move
-    case 3:
-
+    case 3 /*move*/:
+      //altro switch perchè nel caso si selezioni move ci sono 3 opzioni possibili
       switch (sel) {
-        case 1:
+        case 1 /*automatic*/:
           drawMenuElement(4, 2, bitmap_icon_joistick, home_menu_move[2]);
           disp.drawRect(1, 22, 126, 20, WHITE);
           drawMenuElement(4, 24, bitmap_move, home_menu_move[0]);
@@ -148,7 +145,7 @@ void drawMenu2(int i, int sel, int pin_v, float phi, float teta) {
           disp.display();
           break;
 
-        case 2:
+        case 2 /*zenit*/:
           drawMenuElement(4, 2, bitmap_move, home_menu_move[0]);
           disp.drawRect(1, 22, 126, 20, WHITE);
           drawMenuElement(4, 24, bitmap_icon_move3, home_menu_move[1]);
@@ -156,7 +153,7 @@ void drawMenu2(int i, int sel, int pin_v, float phi, float teta) {
           disp.display();
           break;
 
-        case 3:
+        case 3 /*manuale*/:
           drawMenuElement(4, 2, bitmap_icon_move3, home_menu_move[1]);
           disp.drawRect(1, 22, 126, 20, WHITE);
           drawMenuElement(4, 24, bitmap_icon_joistick, home_menu_move[2]);
@@ -167,18 +164,18 @@ void drawMenu2(int i, int sel, int pin_v, float phi, float teta) {
 
       break;
 
-    //history
-   /* case 4:
+    case 4 /*history*/:
       
-      //dtostrf(Tensione(pin_v), 5, 2, tensione_string);
-      //drawMenuElement(4, 2, bitmap_max_irradiance, max_tension_string);
+      dtostrf(Tensione(pin_v), 5, 2, tensione_string);
+      drawMenuElement(4, 2, bitmap_max_irradiance, max_tension_string);
 
-      //dtostrf(Tensione(pin_v), 5, 2, min_tension_string);
-      //drawMenuElement(4, 24, bitmap_min_irradiance, min_tension_string);
-      break;*/
+      dtostrf(Tensione(pin_v), 5, 2, min_tension_string);
+      drawMenuElement(4, 24, bitmap_min_irradiance, min_tension_string);
+      break;
   }
 }
 
+/* Informazioni a schermo dopo aver selezionato una delle tre move */
 void drawMenu3(int i) {
   switch (i) {
     case 1:
@@ -222,7 +219,7 @@ void drawMenu3(int i) {
   disp.display();
 }
 
-
+/* Funzione che seleziona il menu da printare */
 void SelezioneMenu(int i, int j, int sel, int pin_v, float phi, float teta){
   switch(j){
   case 1:
@@ -237,13 +234,5 @@ void SelezioneMenu(int i, int j, int sel, int pin_v, float phi, float teta){
   }
 }
 
-int menu_keep_range_1_3(int val){
-  if(val<1){
-    val = 1;
-  }
-  if(val>3){
-    val = 3;
-  }
-  return val;
-}
+
 
